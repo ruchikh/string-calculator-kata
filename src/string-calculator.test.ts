472e18444f09bb76c9737e77d60e57b797827735
Ruchi <ruchikharwar4@gmail.com>
2025-06-10 11:07:56 +0530

    handle single number input

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,6 +1,11 @@
 import { add } from "./string-calculator"
+
 describe("String Calculator", () => {
-    test("returns 0 for empty string", () => {
+  test("returns 0 for empty string", () => {
     expect(add("")).toBe(0);
+  });
+
+  test("returns number for single input", () => {
+    expect(add("8")).toBe(8);
   });
 });
--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,3 +1,4 @@
-export function add(input: String):number {
+export function add(input: string):number {
   if(!input) return 0;
+  return parseInt(input);
 }
