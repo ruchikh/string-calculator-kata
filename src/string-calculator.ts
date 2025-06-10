f1d9a969639064537735048af5e53dd884a3f786
Ruchi <ruchikharwar4@gmail.com>
2025-06-10 11:22:00 +0530

    handle code for support newline as a seperator

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,6 +1,7 @@
 export function add(input: string):number {
   if(!input) return 0;
-  const splitString = input.split(',');
+  const delimiter = /,|\n/
+  const splitString = input.split(delimiter);
   const numbers = splitString.map(Number)
   return numbers.reduce((acc, val) => acc + val, 0)
 }
